b4d5c5f5ab15c489063433d3258a45f709c42651
akrm al-hakimi <alhakimiakrmj@gmail.com>
2026-03-13 18:53:31 -0400

    feat(cli): `--list` and `--path` args

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,29 @@
 use std::path::PathBuf;
+use clap::Parser;
 use unrot_core::find_broken_symlinks;
 
 fn main() {
-    // FIXME: Result instead
-    let path = std::env::current_dir().unwrap_or_else(|_| PathBuf::from("."));
+    let args = Args::parse();
+
+    // FIXME: Result instead 
+    let path = if args.path == PathBuf::from(".") {
+        std::env::current_dir().unwrap_or_else(|_| PathBuf::from("."))
+    } else {
+        args.path
+    };
+
     let broken = find_broken_symlinks(&path);
     for link in broken {
         println!("{link}");
     }
+}
+
+#[derive(Parser)]
+#[command(name = "unrot")]
+struct Args {
+    #[arg(short, long)]
+    list: bool,
+
+    #[arg(short, long, default_value = ".")]
+    path: PathBuf,
 }
